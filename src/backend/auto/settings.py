180dea0f77d78018ff0dcae89bc503b1c6a75ae5
Oguzhan Yilmaz <oguzhanylmz271@gmail.com>
2020-06-18 23:05:46 +0300

    final changes

--- src/backend/auto/settings.py
+++ src/backend/auto/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0z6_+mf%ehd@rim*!))x4%#^#(+33al7b6ev5qs&adetu-s6-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
